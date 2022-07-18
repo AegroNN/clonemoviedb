94ff49276aeef9f54a5ff3999f5e03fa94347079
Kayra Akbayramlar <kayraakbayramlar@Kayras-MacBook-Pro.local>
2022-07-18 10:18:05 +0300

    First Start

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <movieMain></movieMain>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import movieMain from './components/movieMain.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    movieMain
   }
 }
 </script>
---
+++ src/components/movieMain.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Hello MovieDB</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"movieMain"
+}
+</script>
+
+<style scoped>
+
+</style>
